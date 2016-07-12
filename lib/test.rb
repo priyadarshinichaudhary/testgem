78c948ab05db396f2cd5b4c771b25d24a4443be7
priyadarshini <priyadarshanimini@gmail.com>
2016-07-12 17:16:18 +0530

    make it

--- lib/test.rb
+++ lib/test.rb
@@ -1,8 +1,29 @@
 require 'bundler/setup'
+require 'nokogiri'
+require 'open-uri'
 module Test
   class Hola
-	  def self.hi
-	    @me= "I'm TecOrbian"
+	  def self.process
+	    doc = Nokogiri::HTML(open('http://www.imdb.com/movies-in-theaters/?ref_=nv_mv_inth_1'))
+
+	    @cinemas=[]
+	    @new_release=[]
+	    @title=doc.at_css('.sub-list+ .sub-list h3').text
+	    @head_title=doc.at_css('.header+ .sub-list h3').text
+	    begin
+		  	doc.css('.list_item').each do |item|
+			    @item={}
+		    p"-----------#{item.content}"
+		    @item["title"]= item.at_css('h4 a').text
+		    @item["desc"]= item.at_css('.overview-top .outline').text
+		    @item["rating"]=item.css('.value').text
+		    @item["metascore"]=item.css('.metascore strong').text
+		    @item["review"]=item.css('.metascore a').text
+		    #@item["img"]=item.at_css('.shadowed').attr('src')
+		    @cinemas << @item
+		  rescue
+		  end
+    	end
 	  end
   end
 end
