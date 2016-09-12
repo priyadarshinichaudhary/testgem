189a8a8313e661bc0821b3b13395657125e8911c
priyadarshini <priyadarshanimini@gmail.com>
2016-09-12 18:39:01 +0530

    make it

--- lib/test.rb
+++ lib/test.rb
@@ -1,11 +1,13 @@
 require 'bundler/setup'
 require 'nokogiri'
 require 'open-uri'
+
 module Test
   class Hola
 	  def self.process
 	    doc = Nokogiri::HTML(open('http://www.imdb.com/movies-in-theaters/?ref_=nv_mv_inth_1'))
-
+       
+      @cinema_info={}
 	    @cinemas=[]
 	    @title=doc.at_css('.sub-list+ .sub-list h3').text
 	    @head_title=doc.at_css('.header+ .sub-list h3').text
@@ -19,13 +21,13 @@ module Test
 			    @item["metascore"]=item.css('.metascore strong').text
 			    @item["review"]=item.css('.metascore a').text
 			    #@item["img"]=item.at_css('.shadowed').attr('src')
+			    @cinema_info["title"]=@title
 			    @cinemas << @item
+			    @cinema_info["cinemas"]=@cinemas
 			  rescue
 			  end
     	end
-			
-			@cinemas
-
+			@cinema_info
 	  end
   end
 end
--- test.gemspec
+++ test.gemspec
@@ -18,14 +18,14 @@ Gem::Specification.new do |spec|
   spec.homepage      = "https://github.com/priyadarshinichaudhary/testgem.git"
   spec.license       = "MIT"
 
-   spec.files        ='lib/test.rb'
+  spec.files        ='lib/test.rb'
   #spec.files         = `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
   #spec.bindir        = "exe"
   #spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   #spec.require_paths = ["lib"]
   spec.add_dependency 'nokogiri'
 
-  #spec.add_development_dependency "bundler", "~> 1.8"
-  #spec.add_development_dependency "rake", "~> 10.0"
+  spec.add_development_dependency "bundler", "~> 1.8"
+  spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency 'rspec'
 end
